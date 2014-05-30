1f2c7bf0a1d01cf9efd3d39a0f885acec745490f
Jeremy Ward <jrmy.ward@gmail.com>
2014-05-30 14:54:02 -0700

    Basic public theme setup.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require ocd_theme_boomerang/jquery
 //= require jquery_ujs
+//= require ocd_theme_boomerang/application
 //= require turbolinks
-//= require_tree .
---
+++ app/controllers/content_controller.rb
@@ -0,0 +1,7 @@
+class ContentController < ApplicationController
+
+  def index
+
+  end
+
+end
